c99db36aae0c0e4c253c91b8efd212badea3ab6b
rabin153 <rabinkarki153@gmail.com>
2019-05-07 09:30:06 +0545

    spring boot pet clinic app

---
+++ springguru/src/main/java/springuru/springguru/SpringguruApplication.java
@@ -0,0 +1,13 @@
+package springuru.springguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringguruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringguruApplication.class, args);
+	}
+
+}
